51301a6f26aebe9d0caa69857d6312b106d962db
jn80842 <jnewcomb@eecs.berkeley.edu>
2016-04-01 14:37:12 -0700

    three commands for getting data from board

--- client/littleserial.ino
+++ client/littleserial.ino
@@ -1,3 +1,7 @@
+char sendDataOnce = 'd';
+char sendDataContinuously = 's';
+char stopSendingData = 't';
+
 int incomingByte = 0;
 char buffer[30];
 
@@ -16,7 +20,13 @@ void loop()
  
  if (Serial.available() > 0) {
     incomingByte = Serial.readBytesUntil('\n',buffer,30);
-    Serial.println("{\"rowsLeft\":[\"f\",\"f\",\"f\",1.9,\"f\",\"f\",1.9,\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\"],\"rowsRight\":[\"f\",\"f\",\"f\",\"f\",3.2,\"f\",\"f\",\"f\",\"f\",\"f\",1.45,\"f\",\"f\",0.0,\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\"]}");
+    if (buffer[0] == sendDataOnce) {
+        Serial.println("{\"rowsLeft\":[\"f\",\"f\",\"f\",1.9,\"f\",\"f\",1.9,\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\"],\"rowsRight\":[\"f\",\"f\",\"f\",\"f\",3.2,\"f\",\"f\",\"f\",\"f\",\"f\",1.45,\"f\",\"f\",0.0,\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\",\"f\"]}");
+    } else if (buffer[0] == sendDataContinuously) {
+      // send data continuously
+    } else if (buffer[0] == stopSendingData) {
+      // stop sending data
+    }
   }
 
   
